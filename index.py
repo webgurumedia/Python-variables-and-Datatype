3013e58510a55c4692203cdebc99b331ee3d199d
Webguru Media <68376718+webgurumedia@users.noreply.github.com>
2021-01-13 04:29:24 +0100

    Add files via upload
    Python Variables and Data Types

---
+++ index.py
@@ -0,0 +1,30 @@
+# a = 5
+
+# text data type- string -> a ="class"
+# numeric data type-int, float, complex
+# a = 5
+# b = 5.6
+# comlex_datatype = 567j
+
+
+# sequence data type- list, tuple, range
+# cars = ['Ferrari', 'Range', 'Benz']
+# tuple_datatype = ('Ferrari', 'Range', 'Benz')
+#printing from 1 - 10
+for i in range(1, 11, 2):
+    print(i)
+
+# mapping data type 
+
+couple = dict(a='b', c='d', e='f')
+
+# boolean data type
+
+a = True
+b = False
+
+a = bool(0)
+b = bool(1)
+# set data type 
+a = set(('dog', 'cat', 'monkey'))
+a = frozenset(('dog', 'cat', 'monkey'))
